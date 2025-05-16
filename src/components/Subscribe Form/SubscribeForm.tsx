f9e187f39d5bb00e5d2484f8a3de6c33d6742354
ChenYeh Chien <samddd6718@gmail.com>
2025-05-16 22:04:56 +0800

    building HTML with accessibility

---
+++ src/components/Subscribe Form/SubscribeForm.tsx
@@ -0,0 +1,33 @@
+import { FaCheck } from 'react-icons/fa';
+import './SubscribeForm.scss';
+import image from '../../assets/abstract.jpg'
+
+interface Props {
+    header: string;
+    conditions: string[];
+}
+
+export default function SubscribeForm({ header, conditions }: Props) {
+
+
+    return (
+        <div className='subscribe-form'>
+            <article className='subscribe-form__info'>
+                <h1>{ header }</h1>
+                {
+                    conditions.map(item => {
+                        return (
+                            <ul className='subscribe-form__list--item'>
+                                <FaCheck style={{ color: "#6f5dd5" }}/>
+                                { item }
+                            </ul>
+                        )
+                    })
+                }
+            </article>
+            <figure className='subscribe-form__figure'>
+                <img src={ image }/>
+            </figure>
+        </div>
+    )
+}
