c3c57965235c21546e0a6dc038b05b426d0b0ff9
ChenYeh_Chien <chenyeh_chien@pegatroncorp.com>
2025-05-20 14:10:27 +0800

    add API response error handling

--- src/components/Subscribe Form/SubscribeForm.tsx
+++ src/components/Subscribe Form/SubscribeForm.tsx
@@ -1,13 +1,36 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { FaCheck } from 'react-icons/fa';
 import SuccessMessage from '../Alert Message/Success/SuccessMessage';
 import './SubscribeForm.scss';
-import image from '../../assets/abstract.jpg'
+import image from '../../assets/abstract.jpg';
 
 type Condition = {
     id: number;
     message: string;
 }
+
+type SubscribeResponse = {
+    message: string;
+}
+
+class FetchError extends Error {
+    status: number;
+    statusText: string;
+    body: Record<string, string>;
+
+    constructor(
+        status?: number,
+        statusText?: string,
+        body?: Record<string, string>
+    ) {
+        super();
+        this.status = status === undefined ? 200 : status;
+        this.statusText = statusText === undefined ? "" : statusText;
+        this.body = body === undefined ? { message: "" } : body;
+    }
+}
+
+
 
 interface Props {
     header: string;
@@ -18,10 +41,46 @@ export default function SubscribeForm({ header, conditions }: Props) {
     const [email, setEmail] = useState("");
     const [category, setCategory] = useState("Info");
     const [message, setMessage] = useState("Default message");
+    const [showMessage, setShowMessage] = useState(false);
+    const [validationMessage, setValidationMessage] = useState("");
+    const emailInput = useRef<HTMLInputElement>(null);
 
-    const subscribeToGFE = (email: string) => {
+    const handleInputChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        if (showMessage === true) {
+            setShowMessage(false);
+        }
+
+        if (validationMessage.length > 0) {
+            setValidationMessage("");
+        }
+
+        setEmail(event.target.value);
+    }
+
+    const handleClickButton = () => {
+        if (emailInput.current === null) {
+            return;
+        }
+
+        if (emailInput.current.value.length === 0) {
+            setValidationMessage("Email address is required.");
+            return;
+        }
+
+        if (!emailInput.current.checkValidity()) {
+            setValidationMessage("Please enter a valid email address.");
+            return;
+        }
+
+        setValidationMessage("");
+    }
+
+    const subscribeToGFE = (email: string): Promise<SubscribeResponse> => {
         return new Promise((resolve, reject) => {
-            const url = "https://www.greatfrontend.com/api/projects/challenges/newsletter";
+            const url = 
+                "https://www.greatfrontend.com/api/projects/challenges/newsletter";
 
             fetch(
                 url,
@@ -31,31 +90,58 @@ export default function SubscribeForm({ header, conditions }: Props) {
                         "content-type": "application/json"
                     },
                     body: JSON.stringify({
-                        email
+                        email: "samddd6718"
                     })
                 }
             )
-            .then((response) => response.json())
+            .then((response) => {
+                if (!response.ok) {
+                    return response.json().then(errData => {
+                      throw new FetchError(
+                        response.status,
+                        response.statusText,
+                        errData
+                      )
+                    });
+                }
+
+                return response.json();
+            })
             .then((data) => resolve(data))
-            .catch((error) => reject(error))
+            .catch((error) =>reject(error))
         })
     }
 
     const handleSubscription = async (
         event: React.FormEvent<HTMLFormElement>
     ) => {
-        event.preventDefault();
+        try {
+            event.preventDefault();
 
-        const result = await subscribeToGFE(email)
-        setCategory("Success");
-        setMessage((result as any).message);
+            const result = await subscribeToGFE(email)
+            setCategory("Success");
+            setMessage(result.message);
+            setShowMessage(true);
+        } catch(error) {
+            if (error instanceof FetchError) {
+                setCategory("Error");
+                setMessage(
+                    error.body?.error ?? 
+                    "Fail to subscribe. Please ensure your email is correct or try again later."
+                )
+            }
+        } 
+        
     }
 
     return (
         <div className='subscribe-form'>
-            <SuccessMessage 
-                category={category}
-                message={message}/>
+            {
+                showMessage &&
+                <SuccessMessage 
+                    category={category}
+                    message={message}/>
+            }
             <div className='subscribe-form__content'>
                 <article className='subscribe-form__info'>
                     <h1>{ header }</h1>
@@ -76,16 +162,22 @@ export default function SubscribeForm({ header, conditions }: Props) {
                     <form
                         onSubmit={handleSubscription}>
                         <div className='subscribe-form__input'>
-                            <input 
-                                type='email'
-                                placeholder='Enter your email' 
-                                required
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}/>
+                            <div className='subscribe-form__input--format'>
+                                <input 
+                                    ref={emailInput}
+                                    type='email'
+                                    placeholder='Enter your email' 
+                                    required
+                                    value={email}
+                                    onChange={handleInputChange}/>
+                                <span className='subscribe-form__validation-message'>
+                                    { validationMessage }
+                                </span>
+                            </div>
                             <label>We only send you the best! No spam.</label>
                         </div>
                         <div className='subscribe-form__button'>
-                            <button>Subscribe</button>
+                            <button onClick={handleClickButton}>Subscribe</button>
                         </div>
                     </form>
                 </article>
